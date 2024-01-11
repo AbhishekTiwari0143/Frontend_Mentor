ab40f5e953cc61488ecb5be87a9db6c4d6c0df2d
Abhishek Tiwari <108447004+AbhishekTiwari0143@users.noreply.github.com>
2024-01-11 13:44:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav-links");
+const navShow = document.querySelector(".navShow");
 
 menu.addEventListener("click", () => {
+  navShow.classList.toggle("hidden");
+  console.log("removed hidden Class");
   nav.classList.toggle("show");
 });
